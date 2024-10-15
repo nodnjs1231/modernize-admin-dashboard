099016213261768857361fe78cb090392c415546
WonJaeNo <nodnjs1231@quve.kr>
2024-10-15 23:04:28 +0900

    Bug-fix

--- src/app/(DashboardLayout)/layout/sidebar/NavItem/index.tsx
+++ src/app/(DashboardLayout)/layout/sidebar/NavItem/index.tsx
@@ -29,17 +29,6 @@ interface ItemType {
   level?: number | any;
   pathDirect: string;
 }
-  // service worker Regigster 하기
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register("./sw.js")
-        .then((sw) => {
-            console.log('서비스 워커가 등록됨!', sw);
-        })
-        .catch((err) => {
-            console.log('서비스 워커가 등록이 안됨!', err);
-        });
-}
 
 const NavItem = ({ item, level, pathDirect, onClick }: ItemType) => {
   const Icon = item.icon;
