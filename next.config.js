6eb67d24e56feed9e713a056421dc9fb8adf9e3f
WonJaeNo <nodnjs1231@quve.kr>
2024-10-15 23:27:08 +0900

    Add : Next-PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  // disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
 const nextConfig = {
   reactStrictMode: true,
+  // 여기에 다른 Next.js 설정을 추가할 수 있습니다.
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
